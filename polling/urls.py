5fe233a7d2b7621bae702dd920d920596c90c679
daud1 <davidaaron76@gmail.com>
2021-01-13 13:08:07 +0300

    - setup postgres - userLogin view - add dashboard view

--- polling/urls.py
+++ polling/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('creds.urls.web')),
-    path('', include('voting.urls.web'))
+    path('', include('voting.urls.web'), name='voting')
 ]
